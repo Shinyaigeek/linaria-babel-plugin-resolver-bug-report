8e6abaac674915663933c5235d58c970d86bfa45
Shinobu Hayashi <philispaxil@gmail.com>
2023-09-26 13:36:23 +0900

    Implement css properties generator which uses nodejs buildin module

--- app/components/app.jsx
+++ app/components/app.jsx
@@ -1,8 +1,10 @@
+import { getIcons } from "../tokens/icons";
+
 const React = require("react");
 const { css } = require("@linaria/core")
 
 const App = function() {
-    return <div className={css`color: red;`}>This is error report</div>
+    return <div className={css`background: black; color: red; :after { content: ""; display: block; ${getIcons("logo")}; }`}>This is error report</div>
 }
 
 export default App
---
+++ app/tokens/icons.js
@@ -0,0 +1,17 @@
+const { URL } = require("url");
+
+export const getIcons = function (icon) {
+  const BASE_PATH = "https://linaria.dev/dist/";
+
+  switch (icon) {
+    case "logo": {
+      const url = new URL(`${BASE_PATH}/62f658e6bb6e95783a3b.svg`);
+
+      return {
+        backgroundImage: `url(${url})`,
+        height: `24px`,
+        width: `24px`,
+      };
+    }
+  }
+};
